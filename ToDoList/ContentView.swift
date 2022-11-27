817178b3e984c2c45cb211c922733d2a76405c6b
NomoteteS <nomotetes.onetrue@icloud.com>
2022-11-27 15:00:49 +0200

    Ders 1 sonu

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,19 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email : String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.white
+                .ignoresSafeArea() // Yüzde yüz ekran kapatan özellik
+            VStack { // Yukardan alt tarafa doğru sıralamızı yarar
+                Image("Union")
+                    .resizable()
+                    .frame(width: 187, height: 180) // Boyutlandırma
+                    .padding(.bottom,170)
+                
+                RoundedRectangle(cornerRadius: 12) // Yumuşatılmı dikdörtgen
+                    .stroke(.black) // Çerçeve ayarı yapılıyor
+                    .frame(width: 327, height: 48)
+                    .foregroundColor(Color.blue) // renk ayarı
+                
+
+                HStack(spacing: 10){
+                    Spacer()
+                    Text("Forget Password")
+                        .foregroundColor(Color.gray)
+                        .font(.caption)
+                        .padding(.trailing,24) // .leading : Soldan boşluk vermek için , .trailing: Sağdan boşluk
+                }
+                Button {
+                    // Action
+                } label: {
+                    Text("SIGN UP")
+                        .foregroundColor(.white)
+                        .background {
+                            RoundedRectangle(cornerRadius: 12)
+                                .foregroundColor(Color("F79E89"))
+                                .frame(width: 327, height: 48)
+                        }
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ButtonStyleOne: ButtonStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            Divider()
+            configuration.label
+            }
+        .frame(width: 343, height: 50)
+        .background(Color.clear)
+        .foregroundColor(Color.white)
+        .cornerRadius(8)
     }
 }
---
+++ ToDoList/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ToDoList
+//
+//  Created by NomoteteS on 27.11.2022.
+//
+
+import Foundation
