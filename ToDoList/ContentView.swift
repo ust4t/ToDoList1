407109274aeea574ce39bda4f0d7b10beaa92036
NomoteteS <nomotetes.onetrue@icloud.com>
2022-11-28 21:33:22 +0200

    Not eklenmiş hali

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,30 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var email : String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
     
     var body: some View {
+        //MARK: - Stackler : Objeleri konumlandırma ve UI açısından bize yardımcı olacaktır.
+        // ZStack : Üst üste objeleri konumlandırmamızı sağlar.
+        // VStack : Dikey düzlemde objeleri konumlandırmamızı sağlar.
+        // HStack : Yatay düzlemde objelerin konumlandırmamızı sağlar.
+        
         ZStack {
             Color.white
-                .ignoresSafeArea() // Yüzde yüz ekran kapatan özellik
-            VStack { // Yukardan alt tarafa doğru sıralamızı yarar
-                Image("Union")
-                    .resizable()
-                    .frame(width: 187, height: 180) // Boyutlandırma
-                    .padding(.bottom,170)
+                .ignoresSafeArea() // genellikler renk için kullanılır. XCode tarafından sunulan alanın dışına çıkmamızı sağlar ve tam ekran olmasını sağlar.
+            VStack(spacing: 30) { // Spacing : İçine koyduğumuz objelerin birim uzaklığını belirlemenize sağlar.
+                Image("Union") // Resimleri çağırmamızda kullandığımız etken
+                    .resizable() // Resimlerin tekrardan boyutlandırmamızda yardımcı olan propertydir.
+                    .frame(width: 187, height: 180) // Resimlerin,yazıların vb. gibi çerçeve boyutunu ayarlamış oluruz.
+                    .padding(.bottom,170)  //Padding: Boşluk bırakmanı sağlar || .leading : Soldan boşluk vermek için , .trailing: Sağdan boşluk
                 
-                RoundedRectangle(cornerRadius: 12) // Yumuşatılmı dikdörtgen
-                    .stroke(.black) // Çerçeve ayarı yapılıyor
-                    .frame(width: 327, height: 48)
-                    .foregroundColor(Color.blue) // renk ayarı
-                
-
+                VStack(spacing: 40) {
+                    TextField("Email", text: $email) // Textfield oluştururken @State diye depo oluşturmamız gerekiyor. Bu depo aktaracağımız JSON ile alıcı arasındaki bir yol gibidir. Onu etiket olarak kullanırız.
+                        .frame(width: 310)
+                        .background { // arka tarafını ister daire ister benim gibi kare de koyabilirsiniz. Bu property de istediğiniz çoğu şeyi yapabilirsiniz.
+                            RoundedRectangle(cornerRadius: 12) // Köşeleri yumuşaltılmaya yatkın diktörtgen elde ediyoruz.
+                                .stroke(.black) // Çerçeve çizgisini rengini belirlemek için kullanıldı burada.
+                                .frame(width: 327, height: 48)
+                        }
+                    
+                    SecureField("Password", text: $password)
+                        .frame(width: 310)
+                        .background {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.black)
+                                .frame(width: 327, height: 48)
+                        }
+                }
                 HStack(spacing: 10){
                     Spacer()
                     Text("Forget Password")
                         .foregroundColor(Color.gray)
                         .font(.caption)
-                        .padding(.trailing,24) // .leading : Soldan boşluk vermek için , .trailing: Sağdan boşluk
+                        .padding(.trailing,24)
+                   
                 }
                 Button {
                     // Action
@@ -54,19 +72,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct ButtonStyleOne: ButtonStyle {
-    
-    func makeBody(configuration: Configuration) -> some View {
-        HStack {
-            Divider()
-            configuration.label
-            }
-        .frame(width: 343, height: 50)
-        .background(Color.clear)
-        .foregroundColor(Color.white)
-        .cornerRadius(8)
     }
 }
